8a6655eae79e7f6d5be401526fa9a727f3ee7491
shicy <shicy85@163.com>
2019-05-21 17:14:08 +0800

    动态获取带 hash 值的样式和脚本

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // 2019-03-21
 
+const FileSys = require("fs");
 const VRender = require(__vrender__);
 
 const UIGroup = require("./src/components/group");
 const UIButton = require("./src/components/button");
 const UICheckbox = require("./src/components/checkbox");
 
+
+let distHashValue = "";
+
+if (FileSys.existsSync(__dirname + "/dist")) {
+	var files = FileSys.readdirSync(__dirname + "/dist");
+	if (files && files.length > 0) {
+		let hash = files[0].match(/\.(([0-9]|[a-f]){8})\.(js|css)$/);
+		if (hash && hash[1])
+			distHashValue = "." + hash[1];
+	}
+}
 
 module.exports = {
 	install: function () {
@@ -17,18 +29,15 @@ module.exports = {
 	initPageView: function () {
 		let files = [];
 		if (this._isRenderAsApp) {
-			// files.push("vrender-ui.m.css");
-			files.push("vrender-ui.m.0ea9326b.css");
+			files.push("vrender-ui.m" + distHashValue + ".css");
 		}
 		else {
-			// files.push("vrender-ui.p.css");
-			files.push("vrender-ui.p.0ea9326b.css");
+			files.push("vrender-ui.p" + distHashValue + ".css");
 		}
-		// files.push("vrender-ui.js");
-		files.push("vrender-ui.0ea9326b.js");
+		files.push("vrender-ui" + distHashValue + ".js");
 		files.forEach(file => {
 			file = "file://" + __dirname + "/dist/" + file;
 			this.import(file, {group: "ui", minify: false, index: 0});
 		});
-	},
+	}
 };
