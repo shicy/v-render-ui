99241e5d5effe5abfa8c0f1b3d26af0b936870d0
shicy <shicy85@163.com>
2019-05-29 10:22:06 +0800

    删除 webpack build 文件

--- build/build.js
+++
@@ -1,9 +0,0 @@
-// 构建
-
-require("../src/static/css/style.p.css");
-require("../src/static/css/style.m.css");
-
-require("../src/static/js/init.js");
-require("../src/static/js/base.js");
-
-require("../src/components/group/render.js");
