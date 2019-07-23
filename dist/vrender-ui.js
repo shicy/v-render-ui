09f850e93de32d38449d4b429813414760c23f8e
shicy <shicy85@163.com>
2019-07-23 15:45:38 +0800

    构建

--- dist/vrender-ui.js
+++ dist/vrender-ui.js
@@ -1803,15 +1803,15 @@
   };
 
   UISelect.addItem = function (data, index) {
-    return UI._itemsRender.addItem.call(this, data, index);
+    return UI._items.addItem.call(this, data, index);
   };
 
   UISelect.updateItem = function (data, index) {
-    return UI._itemsRender.updateItem.call(this, data, index);
+    return UI._items.updateItem.call(this, data, index);
   };
 
   UISelect.removeItem = function (data, index) {
-    return UI._itemsRender.removeItem.call(this, data, index);
+    return UI._items.removeItem.call(this, data, index);
   }; // ====================================================
 
 
@@ -11821,6 +11821,439 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   if (frontend) {
     window.UIPaginator = UIPaginator;
     UI.init(".ui-paginator", UIPaginator, Renderer);
+  } else {
+    module.exports = Renderer;
+  }
+})(typeof window !== "undefined");
+"use strict";
+
+// 2019-07-23
+// tabbar
+(function (frontend) {
+  if (frontend && VRender.Component.ui.tabbar) return;
+  var UI = frontend ? VRender.Component.ui : require("../../static/js/init");
+  var Fn = UI.fn,
+      Utils = UI.util; ///////////////////////////////////////////////////////
+
+  var UITabbar = UI.tabbar = function (view, options) {
+    return UI._select.call(this, view, options);
+  };
+
+  var _UITabbar = UITabbar.prototype = new UI._select(false);
+
+  _UITabbar.init = function (target, options) {
+    UI._select.init.call(this, target, options);
+
+    this.tabsView = this.$el.find(".tabs");
+    this.$el.on("tap", ".tab", itemClickHandler.bind(this));
+    this.$el.on("tap", ".close", closeClickHandler.bind(this));
+
+    if (this._isRenderAsApp()) {
+      this.$el.on("touchstart", appTouchHandler.bind(this));
+      this.$el.on("touchmove", appTouchHandler.bind(this));
+      this.$el.on("touchend", appTouchHandler.bind(this));
+    } else {
+      this.$el.on("mousedown", ".btn", btnMouseHandler.bind(this));
+      this.$el.on("mouseup", ".btn", btnMouseHandler.bind(this));
+      this.$el.on("mouseleave", ".btn", btnMouseHandler.bind(this));
+      initResizeEvents.call(this);
+    }
+
+    doLayout.call(this);
+  }; // ====================================================
+
+
+  _UITabbar.close = function (value) {
+    var item = null;
+
+    if (typeof value === "number") {
+      if (value >= 0) item = this.tabsView.find(".tab").eq(value);
+    } else if (Utils.isNotBlank(value)) {
+      item = this.tabsView.find(".tab[name='" + value + "']");
+    }
+
+    if (item && item.length > 0) {
+      var data = this._getItemData(item);
+
+      closeInner.call(this, item);
+      return data;
+    }
+
+    return null;
+  }; // ====================================================
+
+
+  _UITabbar.setSelectedIndex = function (value) {
+    var index = Utils.getIndexValue(value);
+    setActive.call(this, index >= 0 ? this._getItems().eq(index) : null);
+    doLayout.call(this);
+  };
+
+  _UITabbar.removeItemAt = function (index) {
+    this.close(Utils.getIndexValue(index));
+  };
+
+  _UITabbar.isMultiple = function () {
+    return false; // 只能是单选
+  };
+
+  _UITabbar.isClosable = function (value) {
+    if (typeof value == "number") {
+      var item = this._getItemAt(value);
+
+      return item && item.is(".closable");
+    } else if (typeof value == "string") {
+      return this.isClosable(this.getIndexByName(value));
+    }
+
+    return false;
+  };
+
+  _UITabbar.setColsable = function (closable, value) {
+    if (typeof value == "string") {
+      return this.setColsable(closable, this.getIndexByName(value));
+    }
+
+    if (typeof value == "number") {
+      var item = this._getItemAt(value);
+
+      if (item && item.length > 0) {
+        if (Utils.isNull(closable) || Utils.isTrue(closable)) {
+          if (!item.is(".closable")) {
+            item.addClass("closable").append("<i class='close'></i>");
+          }
+        } else if (item.is(".closable")) {
+          item.removeClass("closable").children(".close").remove();
+        }
+      }
+    }
+  }; // ====================================================
+
+
+  _UITabbar._getItemContainer = function () {
+    return this.$el.find(".tabs");
+  };
+
+  _UITabbar._getItems = function () {
+    return this.$el.find(".tab");
+  };
+
+  _UITabbar._getNewItem = function ($, itemContainer, data, index) {
+    return getNewItem.call(this, $, itemContainer, data, index);
+  };
+
+  _UITabbar._renderOneItem = function ($, item, data, index) {
+    return renderOneItem.call(this, $, item, data, index);
+  }; ///////////////////////////////////////////////////////
+
+
+  var Renderer = function Renderer(context, options) {
+    UI._selectRender.call(this, context, options);
+  };
+
+  var _Renderer = Renderer.prototype = new UI._selectRender(false);
+
+  _Renderer.render = function ($, target) {
+    target.addClass("ui-tabbar");
+    var tabbar = $("<div class='bar'></div>").appendTo(target);
+    tabbar.append("<ul class='tabs'></ul>");
+    tabbar.append("<div class='thumb'></div>");
+
+    UI._selectRender.render.call(this, $, target);
+
+    renderButtons.call(this, $, target);
+    return this;
+  }; // ====================================================
+
+
+  _Renderer.isMultiple = function () {
+    return false;
+  }; // ====================================================
+
+
+  _Renderer._renderOneItem = function ($, item, data, index) {
+    renderOneItem.call(this, $, item, data, index);
+  };
+
+  _Renderer._getItemContainer = function ($, target) {
+    return target.find(".tabs");
+  };
+
+  _Renderer._getNewItem = function ($, target) {
+    return getNewItem.call(this, $, target);
+  };
+
+  _Renderer._renderEmptyView = function () {// do nothing
+  };
+
+  _Renderer._renderLoadView = function () {// do nothing
+  }; ///////////////////////////////////////////////////////
+
+
+  var itemClickHandler = function itemClickHandler(e) {
+    var item = $(e.currentTarget);
+    if (item.is(".disabled")) return;
+    this.trigger("itemclick", this._getItemData(item));
+    setActive.call(this, item);
+  };
+
+  var btnMouseHandler = function btnMouseHandler(e) {
+    var _this = this;
+
+    var btn = $(e.currentTarget);
+
+    if (this.btnMouseTimerId) {
+      clearInterval(this.btnMouseTimerId);
+      this.btnMouseTimerId = 0;
+    }
+
+    if (e.type == "mousedown") {
+      var target = this.tabsView;
+      var left = parseInt(target.attr("opt-l")) || 0;
+      var step = ($(e.currentTarget).is(".prev") ? 1 : -1) * 20;
+      this.btnMouseTimerId = setInterval(function () {
+        left += step;
+        scrollTo.call(_this, left);
+      }, 50);
+    }
+  };
+
+  var appTouchHandler = function appTouchHandler(e) {
+    var touch = e.touches && e.touches[0];
+
+    if (e.type === "touchstart") {
+      this.touchData = {
+        startX: touch.pageX
+      };
+      this.touchData.startL = parseInt(this.tabsView.attr("opt-l")) || 0;
+    } else if (e.type === "touchmove") {
+      if (e.touches.length > 1 || !this.$el.is(".over")) return;
+      var offset = touch.pageX - this.touchData.startX;
+      if (!this.touchData.moving && Math.abs(offset) < 10) return;
+      this.touchData.moving = true; // this.touchData.lastOffset = offset;
+
+      var left = this.touchData.startL + offset;
+      scrollTo.call(this, left);
+    } else if (e.type === "touchend") {//
+    }
+  };
+
+  var closeClickHandler = function closeClickHandler(e) {
+    var item = $(e.currentTarget).parent();
+    closeInner.call(this, item);
+    return false;
+  };
+
+  var windowResizeHandler = function windowResizeHandler(e) {
+    if (this.isMounted()) {
+      layoutChanged.call(this);
+    } else {
+      $(window).off("resize._" + this.getViewId());
+    }
+  }; // ====================================================
+
+
+  var renderOneItem = function renderOneItem($, item, data, index) {
+    var box = item.children(".box");
+
+    UI._itemsRender.renderOneItem.call(this, $, item, box, data, index);
+
+    data = data || {};
+    if (Utils.isNotBlank(data.name)) item.attr("name", data.name);
+    if (Utils.isTrue(data.closable)) item.addClass("closable").append("<i class='close'></i>");
+  };
+
+  var getNewItem = function getNewItem($, target) {
+    var item = $("<li class='tab'></li>").appendTo(target);
+    item.append("<div class='box'></div>");
+    return item;
+  };
+
+  var renderButtons = function renderButtons($, target) {
+    target = $("<div class='btns'></div>").appendTo(target);
+    target.append("<span class='btn prev'>&lt;</span>");
+    target.append("<span class='btn next'>&gt;</span>");
+  }; // ====================================================
+
+
+  var initResizeEvents = function initResizeEvents() {
+    var eventName = "resize._" + this.getViewId();
+
+    var _window = $(window).off(eventName);
+
+    _window.on(eventName, windowResizeHandler.bind(this));
+
+    windowResizeHandler.call(this);
+  };
+
+  var doLayout = function doLayout() {
+    layoutChanged.call(this);
+
+    if (this.$el.is(".over")) {
+      // 滚动到可见视图
+      var selectedItem = this.tabsView.find(".tab.selected");
+
+      if (selectedItem && selectedItem.length > 0) {
+        var container = this.tabsView.parent();
+        var itemOffset = selectedItem.offset().left;
+        var containerOffset = container.offset().left;
+        var left = parseInt(this.tabsView.attr("opt-l")) || 0;
+
+        if (itemOffset < containerOffset) {
+          scrollTo.call(this, left + containerOffset - itemOffset);
+        } else if (itemOffset - containerOffset > container.width()) {
+          scrollTo.call(this, left - (itemOffset - containerOffset - container.width() + selectedItem.outerWidth()));
+        }
+      }
+    }
+
+    updateThumb.call(this);
+  };
+
+  var layoutChanged = function layoutChanged() {
+    var target = this.tabsView;
+
+    if (target.width() <= this.$el.width()) {
+      this.$el.removeClass("over over-l over-r");
+      scrollTo.call(this, 0);
+    } else {
+      this.$el.addClass("over");
+      scrollTo.call(this, parseInt(target.attr("opt-l")) || 0);
+    }
+  };
+
+  var scrollTo = function scrollTo(position) {
+    var w1 = this.tabsView.width();
+    var w2 = this.tabsView.parent().width();
+
+    if (position >= 0) {
+      position = 0;
+    } else {
+      if (position + w1 < w2) position = w2 - w1;
+    }
+
+    this.$el.removeClass("over-l over-r");
+    if (position < 0) this.$el.addClass("over-l");
+    if (position + w1 > w2) this.$el.addClass("over-r");
+
+    if (position) {
+      this.tabsView.css("transform", "translate(" + position + "px,0px)");
+      this.tabsView.attr("opt-l", position);
+    } else {
+      this.tabsView.css("transform", "").removeAttr("opt-l");
+    }
+
+    updateThumb.call(this);
+  };
+
+  var updateThumb = function updateThumb() {
+    var _this2 = this;
+
+    if (this.thumbTimerId) {
+      clearTimeout(this.thumbTimerId);
+    }
+
+    this.thumbTimerId = setTimeout(function () {
+      _this2.thumbTimerId = 0;
+
+      var thumb = _this2.$el.find(".thumb");
+
+      var selectedItem = _this2.tabsView.find(".tab.selected");
+
+      if (selectedItem && selectedItem.length > 0) {
+        var left = selectedItem.offset().left;
+        left -= _this2.tabsView.parent().offset().left;
+        var width = selectedItem.outerWidth();
+        var thumbWidth = _this2.options.thumbWidth;
+
+        if (thumbWidth) {
+          if (Utils.isFunction(thumbWidth)) thumbWidth = thumbWidth(selectedItem, width);else if (/%$/.test(thumbWidth)) thumbWidth = parseFloat(thumbWidth) * width;
+
+          if (!isNaN(thumbWidth)) {
+            left += (width - thumbWidth) / 2;
+            width = thumbWidth;
+          }
+        } else if (_this2._isRenderAsApp()) {
+          var innerWidth = selectedItem.width();
+          left += (width - innerWidth) / 2;
+          width = innerWidth;
+        }
+
+        thumb.css("left", left + "px");
+        thumb.width(width);
+      } else {
+        thumb.width(0);
+      }
+    }, 100);
+  };
+
+  var setActive = function setActive(item) {
+    var hasItem = item && item.length > 0;
+    if (hasItem && item.is(".selected")) return;
+    var index = hasItem ? item.index() : [];
+
+    UI._select.setSelectedIndex.call(this, index);
+
+    var oldItem = getSelectedTab.call(this),
+        oldItemData = null;
+
+    if (oldItem && oldItem.length > 0) {
+      oldItem.removeClass("selected");
+      oldItemData = this._getItemData(oldItem);
+      this.trigger("hide", oldItemData);
+    }
+
+    var itemData = null;
+
+    if (hasItem) {
+      item.addClass("selected");
+      itemData = this._getItemData(item);
+      this.trigger("show", itemData);
+    }
+
+    updateThumb.call(this);
+    this.trigger("change", itemData, oldItemData);
+  };
+
+  var closeInner = function closeInner(item) {
+    var isSelected = item.is(".selected");
+
+    var itemData = this._getItemData(item);
+
+    var nextTab = null,
+        nextData = null;
+
+    if (isSelected) {
+      nextTab = item.next();
+      if (!nextTab || nextTab.length == 0) nextTab = item.prev();
+      this.trigger("hide", itemData);
+    }
+
+    this.removeItem(item);
+    this.trigger("close", itemData);
+
+    if (nextTab && nextTab.length > 0) {
+      nextTab.addClass("selected");
+      nextData = this._getItemData(nextTab);
+      this.trigger("show", nextData);
+
+      UI._select.setSelectedIndex.call(this, nextTab.index());
+    } else {
+      UI._select.setSelectedIndex.call(this, []);
+    }
+
+    if (isSelected) this.trigger("change", nextData, itemData);
+    layoutChanged.call(this);
+  };
+
+  var getSelectedTab = function getSelectedTab() {
+    return this.tabsView.find(".tab.selected");
+  }; ///////////////////////////////////////////////////////
+
+
+  if (frontend) {
+    window.UITabbar = UITabbar;
+    UI.init(".ui-tabbar", UITabbar, Renderer);
   } else {
     module.exports = Renderer;
   }
