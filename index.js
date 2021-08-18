82a7c2754cd7c1d08a2454f4ebbfaf845733b16a
shicy <shicy@mixiyun.com>
2021-08-18 22:21:35 +0800

    发布版本1.2.8

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const UITreeSelect = require("./src/components/treeselect");
 
 
 // 前端构建版本，由 gulp 构建脚本自动更新
-const distVersion = "1.2.6";
+const distVersion = "1.2.8";
 
 module.exports = {
 	install: function () {
